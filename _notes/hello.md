cd4e198a5cb6d088e5a9515efe124b111244f014
Koji Shimba <me@shimba.co>
2020-12-30 23:47:35 +0900

    トップページに記事タイトルを表示する

--- _notes/hello.md
+++ _notes/hello.md
@@ -1,7 +1,8 @@
 ---
 title: 'Hello with Markdown!'
 coverImage: '/assets/blog/hello-world/cover.jpg'
-date: '2020-03-16T05:35:07.322Z'
+publishedAt: '2020-03-16T05:35:07.322Z'
+modifiedAt: '2020-03-16T05:35:07.322Z'
 ogImage:
   url: '/assets/blog/hello-world/cover.jpg'
 ---
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,48 @@
+import dayjs from 'dayjs'
+import Link from 'next/link'
+
 import Layout from '~components/layout'
 import Profile from '~components/profile'
 
-function HomePage() {
+import { getAllNotes } from '~lib/note'
+
+function HomePage({ notes }: any) {
   return (
     <Layout>
+      <article className="container mb-7">
+        <div className="justify-content-center row">
+          {
+            notes.map((note: any) => {
+              return (
+                <>
+                  <div className="col-6 text-right">
+                    {dayjs(note.publishedAt).format('YYYY-MM-DD')}
+                  </div>
+                  <div className="col-6">
+                    <Link href={`/${note.slug}`}>
+                      <a>{note.title}</a>
+                    </Link>
+                  </div>
+                </>
+              )
+            })
+          }
+        </div>
+      </article>
       <Profile />
     </Layout>
   )
 }
+
+export async function getStaticProps({ params }: any) {
+  const notes = getAllNotes(['slug', 'publishedAt', 'title'])
+
+  return {
+    props: {
+      notes
+    }
+  }
+}
+
 
 export default HomePage

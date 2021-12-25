a7b8831628ceb554a016bdef35bd52dd0c29e1b6
Koji Shimba <me@shimba.co>
2021-12-26 05:52:44 +0900

    Setup Prisma

---
+++ prisma/migrations/20211225204922_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);

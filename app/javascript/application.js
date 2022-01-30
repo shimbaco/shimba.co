4d83cec93ee7f75f665f565e58efdfff93edfb9c
Koji Shimba <me@shimba.co>
2022-01-31 07:15:44 +0900

    Reformat with Prettier

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
-import { Application } from "@hotwired/stimulus"
-import * as Turbo from "@hotwired/turbo"
-import * as bootstrap from "bootstrap"
+import { Application } from '@hotwired/stimulus';
+import * as Turbo from '@hotwired/turbo';
+import * as bootstrap from 'bootstrap';
 
-import EditorController from './controllers/editor-controller'
-import HelloController from './controllers/hello-controller'
+import EditorController from './controllers/editor-controller';
+import HelloController from './controllers/hello-controller';
 
-window.Stimulus = Application.start()
+window.Stimulus = Application.start();
 
-Stimulus.register("editor", EditorController)
-Stimulus.register("hello", HelloController)
+Stimulus.register('editor', EditorController);
+Stimulus.register('hello', HelloController);
--- app/javascript/editor/index.jsx
+++ app/javascript/editor/index.jsx
@@ -1,29 +1,28 @@
-import debounce from 'lodash/debounce'
-import React, { useCallback, useState } from 'react'
-import { createEditor, Node } from 'slate'
-import { Slate, Editable, withReact } from 'slate-react'
+import debounce from 'lodash/debounce';
+import React, { useCallback, useState } from 'react';
+import { createEditor, Node } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
 
-import eventBus from '../lib/event-bus'
+import eventBus from '../lib/event-bus';
 
 export const Editor = () => {
-  const [editor] = useState(() => withReact(createEditor()))
-  const [value, setValue] = useState([
+  const [editor] = useState(() => withReact(createEditor()));
+  const [value, _setValue] = useState([
     {
       type: 'paragraph',
       children: [{ text: 'A line of text in a paragraph.' }],
     },
-  ])
+  ]);
 
   const emitBody = useCallback(
     debounce((nodes) => {
-      const body = nodes.map((n) => Node.string(n)).join('\n')
-        eventBus.$emit('editor:changed', {
-            body
-        });
-
+      const body = nodes.map((n) => Node.string(n)).join('\n');
+      eventBus.$emit('editor:changed', {
+        body,
+      });
     }, 300),
-    []
-  )
+    [],
+  );
 
   return (
     // Add the editable component inside the context.
@@ -32,10 +31,10 @@ export const Editor = () => {
       value={value}
       onChange={(newNodes) => {
         console.log('newNodes: ', newNodes);
-          emitBody(newNodes)
+        emitBody(newNodes);
       }}
     >
       <Editable />
     </Slate>
-  )
-}
+  );
+};

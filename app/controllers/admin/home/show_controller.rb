759f7cd89e56dc0ab3b2b4a5d49705dc5ef97e4e
Koji Shimba <me@shimba.co>
2022-02-05 12:47:10 +0900

    Update admin layout

--- app/controllers/admin/home/show_controller.rb
+++ app/controllers/admin/home/show_controller.rb
@@ -5,5 +5,13 @@ module Admin::Home
     include Authenticatable
 
     layout "admin"
+
+    def call
+      if shimbaco_signed_in?
+        @posts = Post.order("published_at DESC NULLS FIRST")
+
+        return render("/admin/home/show/call_for_shimbaco")
+      end
+    end
   end
 end

d7657836fc36d7af10544fe9ed486b880c48ec7a
Koji Shimba <me@shimba.co>
2022-02-05 17:54:31 +0900

    Add ability to delete post

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # rubocop:disable Layout/ExtraSpacing
   match "/admin",                     via: :get,    as: :admin,           to: "admin/home/show#call"
   match "/admin/posts",               via: :post,   as: :admin_post_list, to: "admin/posts/create#call"
-  match "/admin/posts/:post_id",      via: :patch,  as: :admin_post,      to: "admin/posts/update#call"
+  match "/admin/posts/:post_id",      via: :delete, as: :admin_post,      to: "admin/posts/destroy#call"
+  match "/admin/posts/:post_id",      via: :patch,                        to: "admin/posts/update#call"
   match "/admin/posts/:post_id/edit", via: :get,    as: :admin_edit_post, to: "admin/posts/edit#call"
   match "/admin/posts/new",           via: :get,    as: :admin_new_post,  to: "admin/posts/new#call"
 
